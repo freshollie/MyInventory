4ff5b2cfed3c23bc6f54e0f708f8a0b5a045a8ce
Oliver Bell <bello@ecm15-mac-04.coventry.ac.uk>
2016-10-31 09:57:33 +0000

    Created app bar, started adding second activity

--- MyInventory/app/src/main/java/uk/ac/coventry/bello/myinventory/AddItemActivity.java
+++ MyInventory/app/src/main/java/uk/ac/coventry/bello/myinventory/AddItemActivity.java
@@ -1,30 +1,21 @@
 package uk.ac.coventry.bello.myinventory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.util.Log;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AddItemActivity extends AppCompatActivity {
-    public final String TAG = "AddItemActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                Log.e(TAG,"Test");
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
