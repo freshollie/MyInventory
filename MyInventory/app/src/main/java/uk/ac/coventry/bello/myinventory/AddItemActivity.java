204da3afc65879886a39357c29739cf0872fe86c
Oliver Bell <bello@ecm15-mac-04.coventry.ac.uk>
2016-11-07 11:00:22 +0000

    Added content to add item activity, created full link between add item activity and main activity

--- MyInventory/app/src/main/java/uk/ac/coventry/bello/myinventory/AddItemActivity.java
+++ MyInventory/app/src/main/java/uk/ac/coventry/bello/myinventory/AddItemActivity.java
@@ -5,8 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.support.v4.app.NavUtils;
 import android.view.View;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.NumberPicker;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -17,6 +22,12 @@ public class AddItemActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NumberPicker np = (NumberPicker) findViewById(R.id.item_quanitity_picker);
+        np.setMinValue(0);
+        np.setMaxValue(99);
+        np.setWrapSelectorWheel(false);
+
     }
 
     @Override
@@ -25,5 +36,44 @@ public class AddItemActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_add_item, menu);
         return true;
     }
+    @Override
+    public void onBackPressed() {
+        /*
+        Called when the user tries to leave the app activity without pressing the save
+        button
+         */
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to leave without saving?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        AddItemActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    private boolean saveItem(){
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case android.R.id.home:
+                this.onBackPressed();
+                return true;
+
+            case R.id.action_done:
+                if (this.saveItem()) {
+                    NavUtils.navigateUpFromSameTask(this);
+                }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
+    }
+
 
 }
